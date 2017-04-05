c10fdbc096fefb7e3a2a1648074a86d50dd75c4c
Pyke1 <tdicquemare@gmail.com>
2017-04-05 15:21:01 +0200

    Effacer efficace

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 window.addEventListener("load",function(){
-	context = document.getElementById('myCanvas').getContext("2d");
-	div = document.getElementById('CanvasBox');
+	document.getElementById("clearCanvas").addEventListener("click", clear);
+	var tool = document.getElementById('Option');
+	var context = document.getElementById('myCanvas').getContext("2d");
+	var div = document.getElementById('CanvasBox');
 	var clickX = new Array();
 	var clickY = new Array();
 	var clickDrag = new Array();
 	var paint;
+
+function clear(){
+	 context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+	  clickX = new Array();
+	  clickY = new Array();
+      clickDrag = new Array();
+	
+}
 
 function addClick(x, y, dragging)
 {
@@ -14,51 +24,48 @@ function addClick(x, y, dragging)
 }
 
 
-document.addEventListener("mousedown", function(e){
-  var mouseX = e.pageX - div.offsetLeft;
-  var mouseY = e.pageY - div.offsetTop;
-  paint = true;
-  addClick(e.pageX - div.offsetLeft, e.pageY - div.offsetTop, false);
-  redraw();
-});
+	document.addEventListener("mousedown", function(e){
+	  var mouseX = e.pageX - div.offsetLeft;
+	  var mouseY = e.pageY - div.offsetTop;
+	  paint = true;
+	  addClick(e.pageX - div.offsetLeft, e.pageY - div.offsetTop, false);
+	  redraw();
+	});
 
-document.addEventListener("mousemove", function(e){
- if(paint){
-    addClick(e.pageX - div.offsetLeft, e.pageY - div.offsetTop, true);
-    redraw();
-  }
-});
+	document.addEventListener("mousemove", function(e){
+	 if(paint){
+		addClick(e.pageX - div.offsetLeft, e.pageY - div.offsetTop, true);
+		redraw();
+	  }
+	});
 
-document.addEventListener("mouseup", function(e){
-	paint = false;
-});
+	document.addEventListener("mouseup", function(e){
+		paint = false;
+	});
 
-document.addEventListener("mouseleave", function(e){
-	paint = false;
-});
+	document.addEventListener("mouseleave", function(e){
+		paint = false;
+	});
 
-document.getElementById("clearCanvas").addEventListener("click", clear);
-function clear(){
-	window.location.replace(window.location.pathname + window.location.search + window.location.hash);
-}
 
-function redraw(){
-  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-  context.strokeStyle = "#000000";
-  context.lineJoin = "round";
-  context.lineWidth = 5;
 
-  for(var i=0; i < clickX.length; i++) {
-    context.beginPath();
-    if(clickDrag[i]){
-      context.moveTo(clickX[i-1], clickY[i-1]);
-     }else{
-       context.moveTo(clickX[i]-1, clickY[i]);
-     }
-     context.lineTo(clickX[i], clickY[i]);
-     context.closePath();
-     context.stroke();
-  }
-}
+
+		function redraw(){
+		  context.strokeStyle = "#000000";
+		  context.lineJoin = "round";
+		  context.lineWidth = 5;
+
+		  for(var i=0; i < clickX.length; i++) {
+			context.beginPath();
+			if(clickDrag[i]){
+			  context.moveTo(clickX[i-1], clickY[i-1]);
+			 }else{
+			   context.moveTo(clickX[i]-1, clickY[i]);
+			 }
+			 context.lineTo(clickX[i], clickY[i]);
+			 context.closePath();
+			 context.stroke();
+		  }
+		}
 
 });
